0a1b3925f4bfc2a570a09d3712ea2d6e95e2aa39
David Bender <bender2242@gmail.com>
2022-09-27 23:46:21 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+        <title>Brickgraph Limited</title>
+        <meta
+          name="description"
+          content="Connectivity and insights for ambitious real estate companies"
+        />
+        <meta name="viewport" content="initial-scale=1 width=device-width" />
+        <link rel="icon" href="/logos/bg-favicon-removebg.png" />
+      </Head>
+  <Component {...pageProps} /></>)
 }
 
 export default MyApp
